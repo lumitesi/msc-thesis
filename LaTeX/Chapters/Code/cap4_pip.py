e2177de62db0911893e4c7a95632b99be3fd5291
Luis Silva <lumitesi@gmail.com>
2018-11-19 02:23:02 +0000

    more rest

---
+++ LaTeX/Chapters/Code/cap4_pip.py
@@ -0,0 +1,20 @@
+[[source]]
+url = "https://pypi.python.org/simple"
+verify_ssl = true
+name = "pypi"
+
+[packages]
+"pyro4" = "==4.62"
+serpent = "==1.23"
+"py-lz4framed" = "==0.10.0"
+python-snappy = "==0.5.1"
+Sphinx = "==1.6.6"
+sphinx_rtd_theme = "==0.2.4"
+flask = "==0.12.2"
+Flask-Jsonpify = "==1.5.0"
+Flask-RESTful = "==0.3.6"
+
+[dev-packages]
+
+[requires]
+python_version = "3.6"
